41a9f91bdd9c8208ea75a68237279ef14994333a
Ramshankar Yadhunath <yadramshankar@gmail.com>
2021-08-05 08:46:22 +0530

    add files for deployment

--- pages/scorer.py
+++ pages/scorer.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
 from pages.helpers import compute_score
 
---
+++ setup.sh
@@ -0,0 +1,11 @@
+python -m spacy download en_core_web_md
+
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
